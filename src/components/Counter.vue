7c031f105251e6e042f22977c1b1e4306116d3b7
abhinav2188 <39943422+abhinavchmod777@users.noreply.github.com>
2019-10-10 13:02:03 +0530

    initial

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="counter">
+    <h2>Count : {{count}}</h2>
+    <button type="button" name="button" @click="count+=1">increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'Counter' ,
+  data(){
+    return{
+      count:0,
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
