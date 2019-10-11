940eb11b88fc01142c450c06d9b58deb3008f1b5
abhinav2188 <39943422+abhinavchmod777@users.noreply.github.com>
2019-10-11 21:00:24 +0530

    game is starting..no audio next sequence on pressing key or using button

---
+++ src/components/audioButton.vue
@@ -0,0 +1,99 @@
+<template lang="html">
+  <div :class="{'pressed':pressed}" :style="{'background-color' :color}" class="btn" @click.prevent="flash()" >
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data : function(){
+    return {
+      pressed : false,
+    }
+  },
+  props : {
+    color : {
+      type : String,
+      required : true,
+    },
+    audioFile : {
+      type : String,
+      required : true
+    },
+    next : {
+      type : String,
+      required : false
+    }
+  },
+  computed : {
+    isNext : function(){
+      return this.next == this.color;
+    }
+  },
+  methods : {
+    playSound : function(){
+        try{
+        var audio = new Audio(this.audioFile);
+        audio.play();
+        }catch(err){
+        console.log(err.message);
+      }
+      },
+    flash : function(){
+      this.pressed = true ;
+      var self = this;
+      setTimeout(function(){
+        self.pressed = false;
+      },100);
+      // this.playSound();
+    },
+    // next : function(color){
+    //   if(this.color == color){
+    //     console.log("in the function")
+    //     console.log(color);
+    //     playSound();
+    //     flash();
+    //   }
+    // }
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+.btn {
+  margin: 25px;
+  display: inline-block;
+  height: 150px;
+  width: 150px;
+  border: 2px solid black;
+  border-radius: 10%;
+}
+
+.game-over {
+  background-color: red;
+  opacity: 0.8;
+}
+
+.red {
+  background-color: red;
+}
+
+.green {
+  background-color: green;
+}
+
+.blue {
+  background-color: blue;
+}
+
+.yellow {
+  background-color: yellow;
+}
+.pressed {
+  box-shadow: 0 0 20px black;
+  /* background-color: grey; */
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+export const eventBus = new Vue({
+  methods : {
+    levelUp : function(nextColor){
+      this.$emit("level-up",nextColor)
+      console.log(nextColor);
+    }
+  }
+});
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
